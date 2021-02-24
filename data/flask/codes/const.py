837fed50cce6a582d05e79b402b87dfd7ebe6e33
wtnb-s <denebu.1.4@gmail.com>
2021-02-24 23:38:11 +0900

    modify scraping program

--- data/flask/codes/const.py
+++ data/flask/codes/const.py
@@ -5,5 +5,6 @@ DATA_PATH_WEATHER = DATA_PATH + "weather/"
 DATA_PATH_MAP = DATA_PATH + "map/"
 DATA_PATH_MAP_JAPAN = DATA_PATH_MAP + "japan-mi.geojson"
 DATA_PATH_CITY = DATA_PATH + "city/city.csv"
+DATA_PATH_PLACE = DATA_PATH + "city/place.csv"
 IMG_PATH = "/api/img/"
 TMP_IMG = 'tmp.png'
--- data/flask/codes/scrapingWeatherData.py
+++ data/flask/codes/scrapingWeatherData.py
@@ -1,34 +1,22 @@
-precNo = [14, 21, 23, 31, 32, 33, 35, 34, 36, 54, 56, 55, 48, 41, 57, 42, 43, 40, 52, 51, 49, 45, 53, 50, 46, 68, 69, 61, 60, 67, 66, 63, 65, 64, 73, 72, 74, 71, 81, 82, 85, 83, 84, 86, 88, 87, 44, 62, 91, 99]
-blockNo = [47412, 47423, 47430, 47575, 47582, 47584, 47588, 47590, 47595, 47604, 47605, 47607, 47610, 47615, 47616, 47624, 47626, 47629, 47632, 47636, 47638, 47648, 47651, 47656, 47670, 47741, 47746, 47759, 47761, 47765, 47768, 47770, 47777, 47780, 47887, 47891, 47893, 47895, 47762, 47807, 47813, 47815, 47817, 47819, 47827, 47830, 47662, 47772, 47936, 89532]
-placeName = ["札幌", "室蘭", "函館", "青森", "秋田", "盛岡", "山形", "仙台", "福島", "新潟", "金沢", "富山", "長野", "宇都宮", "福井", "前橋", "熊谷", "水戸", "岐阜", "名古屋", "甲府", "銚子", "津", "静岡", "横浜", "松江", "鳥取", "京都", "彦根", "広島", "岡山", "神戸", "和歌山", "奈良", "松山", "高松", "高知", "徳島", "下関", "福岡", "佐賀", "大分", "長崎", "熊本", "鹿児島", "宮崎", "東京", "大阪", "那覇", "昭和（南極）"]   
-
-import requests, time
+import requests, time, csv, const, pandas
 from bs4 import BeautifulSoup
-import csv, const
 
 base_url = "http://www.data.jma.go.jp/obd/stats/etrn/view/monthly_s1.php?prec_no=%s&block_no=%s&year=%s&month=&day=1&view=p1"
 
-#取ったデータをfloat型に変換、欠測値の処理も実施
-def str2float(str):
-  try:
-    if (str == '--'):
-      return 0.0
-    else:
-      return float(str)
-  except:
-    return -999.0
+def main():
+  # アメダス地点情報ファイル読み込み
+  amedasPlaceList = pandas.read_csv(const.DATA_PATH_PLACE, header=0)
 
-if __name__ == "__main__":
-  for place in placeName:
+  for place in amedasPlaceList['placeName']:
     print(place)
     #最終的にデータを集めるリスト
     AllList = [['Year', 'Month', 'LLP', 'SLP', 'Precip', 'Temp', 'MaxTemp', 'MinTemp', 'Humidity', 'Wind', 'Daylight', 'Snowfall']]
-    index = placeName.index(place)
+    index = amedasPlaceList['placeName'].index(place)
 
     for year in range(1900,2021):
       print(year)
       #2つの都市コードと年を当てはめる。
-      r = requests.get(base_url%(precNo[index], blockNo[index], year))
+      r = requests.get(base_url%(amedasPlaceList['precNo'][index], amedasPlaceList['blockNo'][index], year))
       r.encoding = r.apparent_encoding
 
       # 対象である表をスクレイピング。
@@ -65,3 +53,16 @@ if __name__ == "__main__":
     with open(const.DATA_PATH_WEATHER + place + '.csv', 'w') as file:
       writer = csv.writer(file, lineterminator='\n')
       writer.writerows(AllList)
+
+#取ったデータをfloat型に変換、欠測値の処理も実施
+def str2float(str):
+  try:
+    if (str == '--'):
+      return 0.0
+    else:
+      return float(str)
+  except:
+    return -999.0
+
+if __name__ == "__main__":
+    main()
